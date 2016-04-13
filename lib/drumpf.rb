bc505a925a1ad90dec4614c062c01184d9070131
Josh W Lewis <josh.w.lewis@gmail.com>
2016-04-13 18:33:43 -0500

    Add some quotes

--- lib/drumpf.rb
+++ lib/drumpf.rb
@@ -1,10 +1,22 @@
 require "drumpf/version"
 
-OFFENSIVE = []
-PRESIDENTAL = []
-PROMISES = []
-LIES = []
-REPEATS = []
+OFFENSIVE = [
+  "Ariana Huffington is unattractive, both inside and out. I fully understand why her former husband left her for a man – he made a good decision."
+  "Robert Pattinson should not take back Kristen Stewart. She cheated on him like a dog & will do it again – just watch. He can do much better!"
+]
+
+PRESIDENTAL = [
+  "An 'extremely credible source' has called my office and told me that Barack Obama’s birth certificate is a fraud."
+]
+
+PROMISES = [
+]
+
+LIES = [
+]
+
+REPEATS = [
+]
 
 module Drumpf
   def self.hi
