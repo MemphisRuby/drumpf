7c35ffb958c5a9d641552272844ce717aaef28ed
Jason Charnes <jason@thecharnes.com>
2016-04-13 18:34:34 -0500

    Add some static methods

--- lib/drumpf.rb
+++ lib/drumpf.rb
@@ -1,13 +1,32 @@
 require "drumpf/version"
 
 OFFENSIVE = []
-PRESIDENTAL = []
+PRESIDENTIAL = []
 PROMISES = []
 LIES = []
 REPEATS = []
 
 module Drumpf
-  def self.hi
-    puts "Hello world!"
+  def self.be_offensive
+    OFFENSIVE.sample
+  end
+
+  def self.be_presidential
+    PRESIDENTIAL.sample
+  end
+
+  def self.give_hope
+    PROMISES.sample
+  end
+
+  def self.lie
+    LIES.sample
+  end
+
+  def self.repeat_yourself
+    "I don't repeat myself. \
+     I don't repeat myself. \
+     Here's the guy who repeats himself. \
+     I don't repeat myself."
   end
 end
